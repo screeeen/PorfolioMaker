44a9581ac880e6c6c4d5ab408a11913147632f9c
screeeen <miguel@screenimplosion.com>
2020-05-07 16:29:20 +0200

    formik on project

--- src/components/ProjectSurvey.jsx
+++ src/components/ProjectSurvey.jsx
@@ -1,90 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import Title from './Title'
-import Info from './Info'
-import Picture from './Picture'
-import Capsule from './Capsule'
-import CapsuleTwoColumn from './CapsuleTwoColumn'
-import VideoClip from './VideoClip'
-import { handleChange } from './utils.jsx'
+import React from 'react'
+import { withFormik, Form, Field } from 'formik'
 
-const ProjectSurvey = ({ data,addProject }) => {
 
-  const initialState = {
-    projectName: '',
-    subtitle: '',
-    picture: undefined,
-    text: '',
-    description: '',
-  }
-
-  const [project, setProject] = useState(initialState);
-
-  useEffect(() => {
-    data.projects.map(project => {
-      console.log('project', project);
-      setProject(project);
-    });
-  }, [data]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // console.log(s);
-    addProject(project);
-  }
+const ProjectSurvey = props => {
 
   return (
     <div className="container-survey">
       <div className="two-col">
         <div className="flex-column">
-          <form onSubmit={handleSubmit}>
-            <label>
-              <p>project name:</p>
-              <input type="text" name='projectName' value={project.projectName} onChange={e => handleChange(project, setProject, e)} />
-            </label>
-            <br />
-            <label>
-              <p>project picture</p>
-              <input type="file" ref={project.fileInput} />
-            </label>
-            <br />
-            <label>
-              <p>subtitle:</p>
-              <input type="text" name='subtitle' value={project.subtitle} onChange={e => handleChange(project, setProject, e)} />
-            </label>
-            <br />
-            <label>
-              <p></p>
-              <textarea name='challengeDescription' value={project.challengeDescription} onChange={e => handleChange(project, setProject, e)} />
-            </label>
-            <br />
-            <p>------------◊◊◊--------------</p>
-            <button type="submit" value="Done" />
-          </form>
-        </div>
-        <div className="container-survey">
-          <p>Project</p>
-          <hr></hr>
-          <section id={project.idTitle}>
-            <Title title={project.projectName} />
-            <Picture picture={project.picture} />
-            <Info projectInfo={project.text} />
-
-            <CapsuleTwoColumn subtitle={project.subtitle} text={project.textTarget} subtitle2={project.subtitleScope} text2={project.textScope} />
-            <Picture picture={project.picture2} />
-
-            <Capsule subtitle={project.subtitleChallenge} text={project.textChallenge} />
-            <Capsule subtitle={project.subtitleProcess} text={project.textProcess} />
-            <VideoClip videoURL={project.clip1} />
-
-            <Capsule subtitle={project.subtitleValidation} text={project.textValidation} />
-            <Capsule subtitle={project.subtitleLearnings} text={project.textLearnings} />
-            <p>{project.challengeDescription}</p>
-            <VideoClip videoURL={project.clip2} />
-          </section>
+          <Form>
+            <Field type="text" name="projectName" placeholder="type your project name..." />
+            <input type="file" name="picture"></input>
+            <Field type="text" name="subtitle" placeholder="type your project name..." />
+            <Field type="text" name="challengeDescription" placeholder="type your project name..." />
+          </Form>
         </div>
       </div>
     </div>
   )
 }
 
-export default ProjectSurvey
+export default withFormik({
+  mapPropsToValue: () => {
+    return {
+      projectName: '',
+      picture: '',
+      text: '',
+      subitle: '',
+      challengeDescription: ''
+    }
+  },
+  handleSubmit: (values, formikBag) => {
+    formikBag.setSubmitting(false);
+    const { addProject } = formikBag.props;
+    addProject(values);
+  }
+
+})(ProjectSurvey)
+
--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,89 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { withFormik, Form, Field } from 'formik'
 import './style.css'
 
-import ProfileCard from './ProfileCard'
-import ProfileForm from './ProfileForm'
-// import MyForm from './MyForm'
 
-const Survey = ({addInfo,data}) => {
-
-
-  const initialProfile = (data.profile ||
-  {
-    name: '',
-    profession: '',
-    fileInput: '',
-    description: '',
-    fruits: ''
-  });
-
-  const [profile, setProfile] = useState(initialProfile);
-
-  useEffect(() => {
-    console.log(data);
-    setProfile(data.profile);
-    // window.localStorage.setItem('profile',data.profile);
-  }, []);
-
-
-  const handleChange = event => {
-    const { name, value } = event.target
-    setProfile({ ...profile, [name]: value });
-  }
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    addInfo(profile);
-  }
-
+const Survey = props => {
 
   return (
-    <div className="container-survey">
-      <ProfileForm  addInfo={addInfo}/>
-      {/* <MyForm  addInfo={addInfo}/> */}
-      {/* <div className="two-col">
-        <div className="flex-column">
-          <form onSubmit={handleSubmit}>
-            <label>
-              <p>Name:</p>
-            </label>
-            <input type="text" name='name' value={profile.name} onChange={handleChange} />
-            <br />
-            <p>Your Best Photo:</p>
-            <input type="file" />
-            <br />
-            <label>
-              <p>What do you do?</p>
-            </label>
-            <input type="text" name='profession' value={profile.profession} onChange={handleChange} />
-            <br />
-            <label>
-              <p>Description:</p>
-              <textarea name='description' value={profile.description} onChange={handleChange} />
-            </label>
-            <br />
-            <label>
-              <p>Pick your favorite flavor:</p>
-              <select value={profile.fruits} name='fruits' onChange={handleChange}>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-              </select>
-            </label>
-            <br />
-            <br />
-            <p>------------◊◊◊--------------</p>
-            <button type="submit" value="Done with the profile part" />
-          </form>
-        </div>
-        <ProfileCard profile ={profile} />
-       
-        </div> */}
-      </div>
-  )
+    <>
+    <Form>
+      {console.log(props)}
+      <Field type="text" name="name" placeholder="Write your name..." />
 
+      <Field type="profession" name="profession" placeholder="Write your profession..." />
+
+      <br />
+      <p>Your Best Photo:</p>
+      <input type="file" />
+      <br />
+      <Field as="textarea" name="description" placeholder="Describe your skills" />
+
+      <Field component="select" name="fruits">
+        <option value="lime">lime</option>
+        <option value="coconut">coconut</option>
+        <option value="mando">mando</option>
+      </Field>
+
+      <button type="submit">Submit</button>
+    </Form>
+    </>
+  );
 }
 
-export default Survey
+export default withFormik({
+  mapPropsToValues: () => {
+    return {
+      name: '',
+      profession: '',
+      file: '',
+      fruits: '',
+    }
+  },
+  handleSubmit: (values, formikBag) => {
+    formikBag.setSubmitting(false);
+    const { addInfo } = formikBag.props
+    console.log('values', values, addInfo);
+    addInfo(values);
+
+  }
+})(Survey)
 
